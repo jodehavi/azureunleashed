56ba6bd26cacc7d0fcdd680f0e1be7a40bce14ae
Steve Griffith <stgriffi@microsoft.com>
2017-05-23 10:30:29 -0400

    removed apt upgrade from setup.sh

--- LabVMSetup/scripts/setup.sh
+++ LabVMSetup/scripts/setup.sh
@@ -2,8 +2,8 @@
 
 logger  "setup.sh - Run apt update"
 sudo apt update
-logger  "setup.sh - Run apt upgrade"
-sudo apt -y upgrade
+#logger  "setup.sh - Run apt upgrade"
+#sudo apt -y upgrade
 
 logger  "setup.sh - Installing Git"
 sudo apt -y install git
