e2b42759dd79ad62c0cd50f5d34435d8325d72d0
Ubuntu <johndh@jdhubuntu.hwonvt0pkaou5cikcyzfwfkazg.bx.internal.cloudapp.net>
2017-05-23 01:55:23 +0000

    changed setup.sh

--- Fabrikam-Bookstore/setup.sh
+++ Fabrikam-Bookstore/setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #Update and Upgrade Ubuntu
-apt-get update
-apt-get upgrade -y
+sudo apt-get update
+sudo apt-get upgrade -y
 
 #Install packages needed by the application
-apt-get install -y apache2 \
+sudo apt-get install -y apache2 \
       php \
       libapache2-mod-php \
       php-mcrypt \
@@ -21,6 +21,10 @@ apt-get install -y apache2 \
 #Run the composer installation
 composer install
 
+#get public ip
+ipaddr=$(curl ipinfo.io/ip)
+echo "Your ip address is $ipaddr"
+
 #setup mysql in azure
 read -p 'admin username: ' uservar
 read -p 'Resource Group Name: ' rgvar
@@ -29,14 +33,35 @@ read -sp 'Password: ' passvar
 az login
 az group create --name $rgvar --location westus
 az mysql server create --resource-group $rgvar --name $snvar --location westus --admin-user $uservar --admin-password $passvar --performance-tier Basic --compute-units 50 --ssl-enforcement Disabled
-#az mysql server firewall-rule create --resource-group $rgvar --server $snvar --name MyClientIp --start-ip-address 0.0.0.0 --end-ip-address 255.255.255.255
+az mysql server firewall-rule create --resource-group $rgvar --server $snvar --name MyClientIp --start-ip-address $ipaddr --end-ip-address $ipaddr
 az mysql server show --resource-group $rgvar --name $snvar
-echo "DB_CONNECTION=mysql" > .env
-echo "DB_HOST="$snvar".mysql.database.azure.com" >> .env
-echo "DB_PORT=3306" >> .env
-echo "DB_DATABASE=fabrikam_bookstore" >> .env
-echo "DB_USERNAME="$uservar"@"$snvar >> .env
-echo "DB_PASSWORD="$passvar >> .env
+#echo "DB_CONNECTION=mysql" > .env
+#echo "DB_HOST="$snvar".mysql.database.azure.com" >> .env
+#echo "DB_PORT=3306" >> .env
+#echo "DB_DATABASE=fabrikam_bookstore" >> .env
+#echo "DB_USERNAME="$uservar"@"$snvar >> .env
+#echo "DB_PASSWORD="$passvar >> .env
+cat <<EOT > .env
+APP_ENV=local
+APP_KEY=
+APP_DEBUG=true
+APP_LOG_LEVEL=debug
+APP_URL=http://localhost
+
+DB_CONNECTION=mysql
+DB_HOST=$snvar.mysql.database.azure.com
+DB_PORT=3306
+DB_DATABASE=fabrikam_bookstore
+DB_USERNAME=$uservar@$snvar
+DB_PASSWORD=$passvar
+EOT
+
+
+echo "sleeping 1 minute to allow ip rules to process"
+sleep 1m # Waits 1 minutes
+#populate the db
+mysql -h $snvar.mysql.database.azure.com -u $uservar@$snvar -p$passvar < dbscripts/master_setup.sql
+mysql -h $snvar.mysql.database.azure.com -u $uservar@$snvar -p$passvar < dbscripts/dbload.sql
 
 #Generate the app key requried to run the site
 php artisan key:generate
