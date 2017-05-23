bf1d90d74df2f8af56fa0004c6dbf3fb7443bfe0
Steve Griffith <stgriffi@microsoft.com>
2017-05-23 17:08:43 -0400

    Update setup.sh
    Fixing cli install

--- LabVMSetup/scripts/setup.sh
+++ LabVMSetup/scripts/setup.sh
@@ -7,12 +7,19 @@ sudo apt update
 
 logger  "setup.sh - Installing Git"
 sudo apt -y install git
+
 logger  "setup.sh - Installing Docker"
 sudo apt -y install docker.io
+
 logger "setup.sh - Installing curl"
 sudo apt -y install curl
+
 logger "setup.sh - Installing Azure CLI"
-sudo apt -y install azure-cli
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ wheezy main" | \
+     sudo tee /etc/apt/sources.list.d/azure-cli.list
+sudo apt-key adv --keyserver packages.microsoft.com --recv-keys 417A0893
+sudo apt-get install apt-transport-https
+sudo apt-get update && sudo apt-get install azure-cli
 
 git clone https://github.com/jodehavi/azureunleashed.git /var/www/azureunleashed
 
