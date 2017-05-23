dd2c92a0214f62a90640598043d07d1f0c1a6dd3
Steve Griffith <stgriffi@microsoft.com>
2017-05-23 08:35:16 -0400

    Updated setup.sh to add logging

--- LabVMSetup/scripts/setup.sh
+++ LabVMSetup/scripts/setup.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+logger  "setup.sh - Run apt update"
 sudo apt update
+logger  "setup.sh - Run apt upgrade"
 sudo apt -y upgrade
 
+logger  "setup.sh - Installing Git"
 sudo apt -y install git
+logger  "setup.sh - Installing Docker"
 sudo apt -y install docker.io
